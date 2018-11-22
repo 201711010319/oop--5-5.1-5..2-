62ddbadf790320061f081918f03f198ff518eb85
201711010319 <201711010319@stu.sdnu.edu.cn>
2018-11-22 22:52:04 +0800

    Add files via upload

---
+++ oop-5(5.1--5.2)/5_01.cpp
@@ -0,0 +1,53 @@
+﻿/*********************************************
+Topic :第五章例5.1
+Author:Qiu xiaoli
+Date  :2018 -11 -16 -18:37:20
+
+**********************************************/
+#include<iostream>
+using namespace std;
+
+class A
+{
+public:
+	A()
+	{
+		cout << "创建A" << endl;
+		getchar();
+	}
+	~A()
+	{
+		cout << "析构A" << endl;
+		getchar();
+	}
+};
+class B
+{
+public:
+	B()
+	{
+		cout << "创建B" << endl;
+		getchar();
+
+	}
+	~B()
+	{
+		cout << "析构B" << endl;
+		getchar();
+
+	}
+private:
+	A a;            //对象成员，B类包含了A类
+};
+
+
+int main()
+{
+
+	B obj;
+
+
+
+system("pause");
+return 0;
+}
---
+++ oop-5(5.1--5.2)/5_02.cpp
@@ -0,0 +1,37 @@
+﻿/*********************************************
+Topic :
+Author:Qiu xiaoli
+Date  :2018 -11 -18 -16:45:08
+
+**********************************************/
+#include<iostream>
+using namespace std;
+class A
+{
+public:
+	A(int a)
+	{
+		x = a;
+		cout << "A:x=" << a << endl;
+	}
+private:
+	int x;
+};
+class B
+{
+public:
+	B(int a, int b, int c) :a2(b), a1(a)         //注意：初始化列表中是成员对象名a1,a2,而不是类名A
+	{
+		y = c;
+		cout << "B:y=" << c << endl;
+	}
+private:
+	A a1, a2;
+	int y;
+};
+int main()
+{
+	B obj(10, 20, 30);
+system("pause");
+return 0;
+}
